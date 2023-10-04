f52290d4cfa950a3270390559efa72aa09248eaf
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-04 12:32:18 +0200

    Ejercicio2.java

---
+++ Ejercicio2.java
@@ -0,0 +1,11 @@
+/* Modifica el programa anterior para que además se muestre tu dirección y
+tu número de teléfono. Asegúrate de que los datos se muestran en líneas
+separadas.*/
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Nuria Rueda Reina");
+        System.out.println("Calle Limonero 23");
+        System.out.println("682343980");
+
+    }
+}
---
+++ Ejercicio3.java
@@ -0,0 +1,17 @@
+/* Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su correspondiente traducción al castellano.
+Las palabras deben estar distribuidas en dos columnas y alineadas a la izquierda. */
+public class Ejercicio3 {
+    public static void main(String[] args){
+        System.out.printf("%-10s %s\n", "manzanas", "apple");    
+        System.out.printf("%-10s %s\n", "ordenador", "computer");    
+        System.out.printf("%-10s %s\n", "estudiar", "study");    
+        System.out.printf("%-10s %s\n", "falda", "skirt");    
+        System.out.printf("%-10s %s\n", "feliz", "happy");    
+        System.out.printf("%-10s %s\n", "triste", "sad");    
+        System.out.printf("%-10s %s\n", "pelicula", "movie");    
+        System.out.printf("%-10s %s\n", "lunes", "monday");    
+        System.out.printf("%-10s %s\n", "martes", "tuesday");    
+        System.out.printf("%-10s %s\n", "miercoles", "wednesday");    
+
+    }
+}
