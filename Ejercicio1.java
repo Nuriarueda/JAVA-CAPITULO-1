cc64cb36a390c7353d351fcc7f5d554b028c9d31
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-04 12:27:21 +0200

    Ejercicio1.java

---
+++ Ejercicio1.java
@@ -0,0 +1,8 @@
+/* Escribe una programa que muestre tu nombre por pantalla. */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Hola Nuria Rueda Reina");
+    }
+
+}
+
