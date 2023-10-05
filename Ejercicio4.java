c824ba1ea210150ddc1dc21ca61554024b72a0c5
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-05 09:11:04 +0200

    capitulo 1

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,8 +1,24 @@
 /* Escribe un programa que muestre tu horario de clase. */
 public class Ejercicio4 {
     public static void main (String[] args){
-        
-        
+
+        System.out.println("        ┌───────┬────────┬───────────┬────────┬─────────┐");
+        System.out.println("        │ Lunes │ Martes │ Miércoles │ Jueves │ Viernes │");
+        System.out.println("┌───────├───────┼────────┼───────────┼────────┼─────────┤");
+        System.out.println("│ 8:15  │ PROG  │        │           │  PROG  │         │");
+        System.out.println("├───────┼───────┼────────┼───────────┼────────┼─────────┤");
+        System.out.println("│ 9:15  │ PROG  │        │           │  PROG  │         │");
+        System.out.println("├───────├───────┼────────┼───────────┼────────┼─────────┤");
+        System.out.println("│ 10:15 │ PROG  │  BBDD  │   PROG    │  PROG  │         │");
+        System.out.println("├───────┼───────┼────────┼───────────┼────────┼─────────┤");
+        System.out.println("│ 11:15 │  R    │   E    │    CR     │   E    │    O    │");
+        System.out.println("├───────┼───────┼────────┼───────────┼────────┼─────────┤");
+        System.out.println("│ 11:45 │       │  BBDD  │   PROG    │  BBDD  │         │");
+        System.out.println("├───────┼───────┼────────┼───────────┼────────┼─────────┤");
+        System.out.println("│ 12:45 │       │        │   BBDD    │  BBDD  │         │");
+        System.out.println("├───────┼───────┼────────┼───────────┼────────┼─────────┤");
+        System.out.println("│ 13:45 │       │        │   BBDD    │        │         │");
+        System.out.println("└───────┴───────┴────────┴───────────┴────────┴─────────┘");
     }
 }
 
