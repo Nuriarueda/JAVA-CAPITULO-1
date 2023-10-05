eb2949e379e84057671fbe7ec4ff2e7aac931593
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-05 09:55:56 +0200

    Capitulo 1

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,3 +1,5 @@
+/* Igual que el programa anterior, pero esta vez la pirámide debe aparecer
+invertida, con el vértice hacia abajo. */
 public class Ejercicio8 {
     public static void main(String[] args) {
         
