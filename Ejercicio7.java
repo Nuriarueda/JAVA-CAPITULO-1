21d9953e813425131b7950fcee556e5532870d03
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-05 09:55:01 +0200

    capitulo 1

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -1,3 +1,5 @@
+/* Igual que el programa anterior, pero esta vez la pirámide estará hueca (se debe
+ver únicamente el contorno hecho con asteriscos). */
 public class Ejercicio7 {
     public static void main(String[] args) {
         
