71cb27f0d4b84109152dce1958e005f92cfebf5b
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-05 10:03:28 +0200

    capitulo 1

---
+++ Ejercicio10.java
@@ -0,0 +1,20 @@
+/* Mejora el ejercicio anterior añadiéndole colores */
+
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        
+        System.out.println();
+        System.out.println("   \033[95mAPRENDE JAVA\033[37m                \033[93m██\033[37m ");
+        System.out.println("  \033[95mCON EJERCICIOS\033[37m             \033[93m██\033[37m");
+        System.out.println("                               \033[93m██\033[37m");
+        System.out.println();
+        System.out.println(" \033[95mNuria Rueda Reina\033[37m     \033[94m███████████████████\033[37m");
+        System.out.println("                       \033[94m██████████████   ██\033[37m");
+        System.out.println("                       \033[94m███████████████████\033[37m");
+        System.out.println("                       \033[94m██████████████\033[37m");
+        System.out.println("                       \033[94m██████████████\033[37m");
+        System.out.println("                       \033[94m██████████████\033[37m");
+
+
+    }
+}
