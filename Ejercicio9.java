4f3773f76be3f9deb40381446a871b8468f2d157
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-05 09:56:36 +0200

    capitulo 1

--- Ejercicio9.java
+++ Ejercicio9.java
@@ -1,5 +1,8 @@
+/* Escribe un programa que pinte por pantalla alguna escena - el campo, la
+habitación de una casa, un aula, etc. - o algún objeto animado o inanimado
+- un coche, un gato, una taza de café, etc. Ten en cuenta que puedes utilizar
+caracteres como *, +, <, #, @, etc. o incluso caracteres Unicode. ¡Échale imaginación! */
 public class Ejercicio9 {
-    
     public static void main(String[] args) {
         
         System.out.println();
