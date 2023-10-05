ba368823f81f2828070c6740776239ff35f796b2
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-05 09:54:23 +0200

    capitulo 1

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,3 +1,5 @@
+/* Escribe un programa que pinte por pantalla una pirámide rellena a base de asteriscos. 
+La base de la pirámide debe estar formada por 9 asteriscos. */
 public class Ejercicio6 {
     public static void main(String[] args) {
         
---
+++ Ejercicio9.java
@@ -0,0 +1,19 @@
+public class Ejercicio9 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println();
+        System.out.println("   APRENDE JAVA                ██ ");
+        System.out.println("  CON EJERCICIOS             ██");
+        System.out.println("                               ██");
+        System.out.println();
+        System.out.println("Nuria Rueda Reina      ███████████████████");
+        System.out.println("                       ██████████████   ██");
+        System.out.println("                       ███████████████████");
+        System.out.println("                       ██████████████");
+        System.out.println("                       ██████████████");
+        System.out.println("                       ██████████████");
+
+
+    }
+}
